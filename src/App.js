0d59a5a561df05f63b0c4e999a587cf685ce4dd1
yryskul <kuttubaevayryskul@gmail.com>
2022-03-28 15:44:29 +0600

    added component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Link from "./components/Link/Link"
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  const rightLinks = ["link3","link4","link5"];
+  const leftLinks = ["link1","link2"];
+
+return (
+  <div className="App">
+    <ul>
+      {rightLinks}
+      {leftLinks}
+    </ul>
+  </div>
+)
+
 }
+
 
 export default App;
