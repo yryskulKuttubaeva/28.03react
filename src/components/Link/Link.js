bf7e179b8b520a202a04728e49d1d4eeeb1f44c4
yryskul <kuttubaevayryskul@gmail.com>
2022-03-29 20:25:38 +0600

    add com

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,30 +1,29 @@
-import "./Link.css"
+import "./Link.css";
+function Link() {
+  const rightLinks = ["link3", "link4", "link5"];
+  const leftLinks = ["link1", "link2"];
 
-function App() {
-    const rightLinks = ["link3","link4","link5"];
-    const leftLinks = ["link1","link2"];
-  
-    const left = leftLinks.map((leftLink)=> {
-      return (
-        <li>
-          {leftLink}
-        </li>
-      );
-    })
-  
-    const right = rightLinks.map((rightLink)=> {
-      return(
-        <li>
-        {rightLink}
-        </li>
-      );
-    })
-    return(
-        <div className="App">
-             <ul>
-                 {left}
-                 {right}
-             </ul>
-        </div>
-    )}
+  const left = leftLinks.map((leftlink) => {
+    return (
+      <li>
+        <a href="#">{leftlink}</a>
+      </li>
+    )
+  })
+  const right = rightLinks.map((rightlink) => {
+    return (
+      <li>
+        <a href="#">{rightlink}</a>
+      </li>
+    )
+  })
+
+return (
+<div className="Div">
+  <ul>{left} {right}</ul>
+ 
+</div>
+)
+}
+
 export default Link;
